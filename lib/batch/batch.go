1fb440152b6339190e4a544d10410676463e8905
alexhaiduk <alexhaiduk82@gmail.com>
2022-07-06 23:27:59 +0300

    not work

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -16,19 +16,32 @@ func getOne(id int64) user {
 
 func getBatch(n int64, pool int64) (res []user) {
 	var wg sync.WaitGroup
-	//var mu sync.Mutex
-	ch := make(chan []user, pool)
-	//res = make([]user, 0)
+	var mu sync.Mutex
+	//ch := make(chan []user, pool)
+	res = make([]user, 0, n)
+	//wg.Add(int(pool))
 	for i := 0; i < int(n); i++ {
-		wg.Add(1)
-		go func(j int64) {
+		wg.Add(int(pool))
+		//go func(j int64) {
+		go func() {
+			defer wg.Done()
+			mu.Lock()
+			//tmp := getOne(j)
+			//res = append(res, tmp)
+			res = append(res, getOne(int64(i)))
 			//res = append(res, getOne(j))
-			//mu.Lock()
-			ch <- append(<-ch, getOne(j))
-			//mu.Unlock()
-		}(int64(i))
-		wg.Done()
+			//ch <- append(res, getOne(int64(j)))
+			//ch <- res
+			mu.Unlock()
+			//ch <- res
+			//}(int64(i))
+			//ch <- res
+		}()
+		//wg.Done()
 	}
 	wg.Wait()
-	return <-ch
+	//return <-ch
+	//res = <-ch
+	//return res
+	return res
 }
